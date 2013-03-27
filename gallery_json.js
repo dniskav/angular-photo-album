b27c8f11c12915e0fb568812a8918693d5718ad8
Daniel Silva Bosz2013 <daniel.silva@boszdigital.com>
2013-03-27 09:23:13 -0500

    add a basic sort field

--- gallery_json.js
+++ gallery_json.js
@@ -8,7 +8,11 @@
       "url":"img/image1.jpg",
       "thumb_url":"img/thumb1.jpg",
       "title":"Julie sitting on the grass",
-      "date":"December 12, 2008",
+      "date":{
+        "month" : "December",
+        "day" : 12, 
+        "year" :2008
+      },
       "location":"Los Angeles, California"
     },
     {
@@ -16,7 +20,11 @@
       "url":"img/image2.jpg",
       "thumb_url":"img/thumb2.jpg",
       "title":"Shoes",
-      "date":"January 1, 2008",
+      "date":{
+        "month" : "January",
+        "day" : 1, 
+        "year" :2008
+      },
       "location":"Tokyo, Japan"
     },
     {
@@ -24,7 +32,11 @@
       "url":"img/image3.jpg",
       "thumb_url":"img/thumb3.jpg",
       "title":"Jessica and Amy by the lake",
-      "date":"February 26, 2008",
+      "date":{
+        "month" : "February",
+        "day" : 26, 
+        "year" :2008
+      },
       "location":"Paris, France"
     },
     {
@@ -32,7 +44,11 @@
       "url":"img/image4.jpg",
       "thumb_url":"img/thumb4.jpg",
       "title":"Mike and Julie",
-      "date":"May 1, 2008",
+      "date":{
+        "month" : "May",
+        "day" : 1, 
+        "year" :2008
+      },
       "location":"New York, New York"
     },
     {
@@ -40,7 +56,11 @@
       "url":"img/image5.jpg",
       "thumb_url":"img/thumb5.jpg",
       "title":"Canoe!",
-      "date":"March 13, 2008",
+      "date":{
+        "month" : "March",
+        "day" : 13, 
+        "year" :2008
+      },
       "location":"London, England"
     },
     {
@@ -48,7 +68,11 @@
       "url":"img/image6.jpg",
       "thumb_url":"img/thumb6.jpg",
       "title":"The girls in the tent",
-      "date":"July 4, 2008",
+      "date":{
+        "month" : "July",
+        "day" : 4, 
+        "year" :2008
+      },
       "location":"Dubai, United Arab Emirates"
     }
   ]
